0eebdeda4d6721890f3799b23f473529855513cd
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-18 15:29:02 -0500

    function to run through quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,8 +23,61 @@ let strtQuiz = function () {
     runQs();
 };
 
-
 // function for run questions
 function runQs() {
-    
+    let questionAreaContentElement = document.createElement("section");
+    questionAreaContentElement.id = "question-area-content"
+    document.body.append(questionAreaContentElement);
+
+    let currentQuest = 0
+    let questionHeader = document.createElement("h3");
+    questionAreaContentElement.append(questionHeader)
+
+    let addBtn = function () {
+        for (i = 0; i < questionData[currentQuest].choices.length; i++) {
+            let button = document.createElement("button");
+            button.className = "btn"
+            questionAreaContentElement.append(button);
+        }
+    }
+    addBtn();
+
+    let choicesElement = document.querySelectorAll(".btn");
+
+    function iterate() {
+        questionHeader.textContent = questionData[currentQuest].question
+        choicesElement.forEach((choicesButton, i) => {
+            choicesButton.textContent = questionData[currentQuest].choices[i]
+        })
+    }
+    choicesElement.forEach(el => {
+        el.addEventListener("click", function (event) {
+            let choice = this.textContent
+            if (choice === questionData[currentQuest].answer) {
+                let correctAns = document.createElement("h3")
+                correctAns.textContent = "Correct"
+                $(correctAns).delay(200).fadeOut(500);
+                document.body.append(correctAns)
+            } else {
+                let incorrectAns = document.createElement("h3")
+                incorrectAns.textContent = "Incorrect"
+                $(incorrectAns).delay(200).fadeOut(500);
+                document.body.append(incorrectAns)
+                wrongAnswer();
+            }
+            currentQuest++;
+            if (currentQuest < questionData.length) {
+                iterate()
+            } else {
+                setTimeout(returnScore, 1000);
+            }
+        }
+        );
+
+
+
+
+        iterate();
+
+    })
 }
