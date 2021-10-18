4acf38f850d697cbd0bf4d6f7d06f37ed7ad2e13
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-18 14:04:35 -0500

    quiz questions and answers JS build

---
+++ assets/js/QA.js
@@ -0,0 +1,25 @@
+let questionData = [{
+    question: "Commonly used data types DO Not Include:",
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+},
+{
+    question: "The condition in an if / else statement is enclosed with _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis"
+},
+{
+    question: "Arrays in JavaScript can be used to store",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above."],
+    answer: "all of the above."
+},
+{
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "parenthesis"
+},
+{
+    question: "A very useful tool used during development and debuggin for printing content to debugger is:",
+    choices: ["javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
+}]
