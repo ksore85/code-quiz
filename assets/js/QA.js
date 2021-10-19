5b7ee38a4b13c6d4ef1f71745c34a04968b47928
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-18 21:59:55 -0500

    question touches

--- assets/js/QA.js
+++ assets/js/QA.js
@@ -1,7 +1,7 @@
 let questionData = [{
     question: "Commonly used data types DO Not Include:",
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    answer: 'alerts'
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
 },
 {
     question: "The condition in an if / else statement is enclosed with _____.",
@@ -9,7 +9,7 @@ let questionData = [{
     answer: "parenthesis"
 },
 {
-    question: "Arrays in JavaScript can be used to store",
+    question: "Arrays in JavaScript can be used to store:",
     choices: ["numbers and strings", "other arrays", "booleans", "all of the above."],
     answer: "all of the above."
 },
